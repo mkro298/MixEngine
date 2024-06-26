1c3dc5f964bc6dbaf4d451395b59d02355f07abb
Monisha Krothapalli <mkrothapalli123@gmail.com>
2024-06-26 04:29:09 -0700

    basic flask works to authenticate log in and extract user playlists

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os 
-from flask import Flask, session
+from flask import Flask, session, url_for, redirect, request
 from dotenv import load_dotenv
 
 from spotipy import Spotify
@@ -13,7 +13,7 @@ app.config['SECRET_KEY'] = os.urandom(64)
 
 client_id =  os.getenv("CLIENT_ID")
 client_secert = os.getenv("CLIENT_SECRET")
-redirect_url = 'http://localhost:3000/'
+redirect_url = 'http://127.0.0.1:5000/callback'
 scope = 'playlist-read-private, playlist-modify-private'
 
 cache_handler = FlaskSessionCacheHandler(session)
@@ -28,6 +28,34 @@ sp_oauth = SpotifyOAuth(
 
 sp = Spotify(auth_manager=sp_oauth)
 
+@app.route('/')
+def home(): 
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for('get_playlists'))
+
+@app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_playlists'))
+
+@app.route('/get_playlists')
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    playlists = sp.current_user_playlists()
+    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
+    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
+
+    return playlists_html
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
