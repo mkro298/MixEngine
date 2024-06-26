fae54efd1a4f9397e421594b8d4e11e7ae8c469d
Monisha Krothapalli <mkrothapalli123@gmail.com>
2024-06-26 15:12:51 -0700

    create_playlist added

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
 
 client_id =  os.getenv("CLIENT_ID")
-client_secert = os.getenv("CLIENT_SECRET")
+client_secret = os.getenv("CLIENT_SECRET")
 redirect_url = 'http://127.0.0.1:5000/callback'
-scope = 'playlist-read-private, playlist-modify-private'
+scope = 'playlist-read-private playlist-modify-private'
 
 cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
     client_id=client_id, 
-    client_secret=client_secert, 
+    client_secret=client_secret, 
     redirect_uri=redirect_url, 
     scope=scope, 
     cache_handler=cache_handler, 
@@ -45,12 +45,10 @@ def get_playlists():
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
+    user_info = sp.me()
+    sp.user_playlist_create(user=user_info['id'], name="play", public=False, description = " ", collaborative=False)
+    return f"Playlist created for user:{user_info['display_name']}"
     
-    playlists = sp.current_user_playlists()
-    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
-    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
-
-    return playlists_html
 
 @app.route('/logout')
 def logout():
