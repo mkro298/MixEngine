6b423deea4c34dca879407e8cf82018b824aef28
Monisha Krothapalli <mkrothapalli123@gmail.com>
2024-07-04 00:51:10 -0700

    Update main.py

--- react-flask/flask-server/main.py
+++ react-flask/flask-server/main.py
@@ -86,5 +86,6 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
